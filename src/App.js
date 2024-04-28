ece72e1cc2ffd03605cea1d4cd46d4298c0aeac9
Hrish-g <hrishikeshrgupta@gmail.com>
2024-04-28 13:02:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Cctv from './components/Cctv';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Cctv/>
     </div>
   );
 }
 
 export default App;
+// in firebase my realtime database looks like 
+// |-parking_slots
+//    |-slot1
+//        |-booked_by:"null"
+//        |-slot_number:1
+//        |-status:"available"
+//   |-slot2
+// |-users
+//     |-2T15y2XA31coxOGfeqdDqaSaQjc2
+//          |-email:"vu4f2021069@pvppcoe.ac.in"
+//          |-name:"lavkush"
+//          |-timestamps
+//              |-0
+//                   |-entry:"2024-04-04T06:36:05.614Z"
+//                   |-exit:"2024-04-04T06:36:13.972Z"
+//             |-1
+//                   |-entry:"2024-04-04T06:36:18.326Z"
+//                   |-exit:"2024-04-04T06:36:28.925Z"
+//       |-vehicle:"MH12DE1433"
+//    |-q4b0p4rdJuY3a8ZF7H9o6UF0MvH3
+
+
